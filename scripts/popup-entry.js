ceb745dea5b33c78d77ac4eeca78cb7e108435cf
Robert <rob.dunn@gmail.com>
2014-11-23 19:16:46 -0500

    Initial Commit

---
+++ scripts/popup-entry.js
@@ -0,0 +1,7 @@
+$(function () {
+  BG.hideDesktopNotification();
+  getSetting(SETTINGS.animations_disabled) && ($.fx.off = !0);
+  applyLocalization();
+  setUpHandlers();
+  fillNotifications()
+});
